6285ea55de6a60aff284857c1ba8fa807793c381
shivasaiacademy <shivasaitechnicalacademy@gmail.com>
2024-10-01 19:02:06 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,11 @@
+Write a program to print “Welcome to programming in C laboratory” 
+
+#include <stdio.h>
+
+int main() {
+    printf("Welcome to programming in C laboratory\n");
+    return 0;
+}
+
+Output:
+Welcome to programiing in C laboratory
