16b182af92d6116036bd95882c6b83b09edde5f6
shivasaiacademy <shivasaitechnicalacademy@gmail.com>
2024-10-01 19:09:31 +0530

    Create 5.c

---
+++ 5.c
@@ -0,0 +1,24 @@
+Write a program to find even or odd number
+
+#include <stdio.h>
+
+int main() {
+    int number;
+
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+
+    if (number % 2 == 0) {
+        printf("%d is an even number.\n", number);
+    } else {
+        printf("%d is an odd number.\n", number);
+    }
+
+    return 0;
+}
+
+Input:
+Enter an integer: 5
+
+Output:
+5 is an odd number.
